3129581d19b14b72f7389152ef83561ddff0070d
BrettLennox <brettheaton@hotmail.com>
2022-05-06 13:34:14 +1000

    Items Script and Inventory filled

--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -25,7 +25,7 @@ public static class ItemData
                 _amount = 1;
                 _icon = "Food/Apple";
                 _type = ItemTypes.Food;
-                _heal = 10;
+                _heal = 2;
                 break;
             case 1:
                 _name = "Meat";
@@ -38,20 +38,146 @@ public static class ItemData
                 break;
             #endregion
             #region Weapon 100-199
+            case 100:
+                _name = "Axe";
+                _description = "Heavy but deadly";
+                _value = 1;
+                _amount = 1;
+                _icon = "Weapon/Axe";
+                _type = ItemTypes.Weapon;
+                _heal = 10;
+                break;
             #endregion
             #region Apparel 200-299
+            case 200:
+                _name = "Gold Ring";
+                _description = "Protects a very small amount of your finger";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Ring";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 201:
+                _name = "Bronze Armour";
+                _description = "Keeps your body protected. Slightly";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 202:
+                _name = "Steel Armour";
+                _description = "Just a bit better at protecting your body";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour1";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 203:
+                _name = "Wizard Armour";
+                _description = "Armour for wizards. It looks cool but probably won't do much against a blade";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour2";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 204:
+                _name = "Mithril Armour";
+                _description = "Protects much better and looks nice";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour3";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 205:
+                _name = "Black Armour";
+                _description = "Good at protecting from hits and hiding in the shadows";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour4";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
+            case 206:
+                _name = "Elite Mithril Armour";
+                _description = "Damn good looking armour";
+                _value = 1;
+                _amount = 1;
+                _icon = "Apparel/Armour5";
+                _type = ItemTypes.Apparel;
+                _heal = 0;
+                break;
             #endregion
             #region Crafting 300-399
+            case 300:
+                _name = "Ingot";
+                _description = "Useful for creating armour and weapons";
+                _value = 1;
+                _amount = 1;
+                _icon = "Crafting/Ingots";
+                _type = ItemTypes.Crafting;
+                _heal = 0;
+                break;
             #endregion
             #region Ingredients 400-499
             #endregion
             #region Potions 500-599
+            case 500:
+                _name = "Health Potion";
+                _description = "Restores health";
+                _value = 1;
+                _amount = 1;
+                _icon = "Potions/HP";
+                _type = ItemTypes.Potion;
+                _heal = 20;
+                break;
+            case 501:
+                _name = "Mana Potion";
+                _description = "Restores mana";
+                _value = 1;
+                _amount = 1;
+                _icon = "Potions/MP";
+                _type = ItemTypes.Potion;
+                _heal = 10;
+                break;
             #endregion
             #region Scrolls 600-699
+            case 600:
+                _name = "Scroll";
+                _description = "Contains a small amount of magical power";
+                _value = 1;
+                _amount = 1;
+                _icon = "Scrolls/Scroll";
+                _type = ItemTypes.Scroll;
+                _heal = 0;
+                break;
             #endregion
             #region Quest 700-799
+            case 700:
+                _name = "Gem";
+                _description = "Very valuable";
+                _value = 1;
+                _amount = 1;
+                _icon = "Quest/Gem";
+                _type = ItemTypes.Quest;
+                _heal = 10;
+                break;
             #endregion
             #region Misc 800-899
+            case 800:
+                _name = "Coin";
+                _description = "Shiney!";
+                _value = 1;
+                _amount = 1;
+                _icon = "Misc/Coins";
+                _type = ItemTypes.Money;
+                _heal = 0;
+                break;
             #endregion
             default:
                 _itemId = 0;
--- Assets/Scripts/Inventory/Player/Inventory.cs
+++ Assets/Scripts/Inventory/Player/Inventory.cs
@@ -1,20 +1,198 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;//lists and dictionaries
 using UnityEngine;
 namespace Inventory.Player
 {
     public class Inventory : MonoBehaviour
     {
+        #region Variables
+        public static List<Item> playerInv = new List<Item>();
+        public Item selectedItem;
+        public static bool showInv;
+        public static int money;
+
+        public Vector2 scrollPos;
+        public string sortType = "All";
+        public string[] enumTypesForItems = { "All", "Food", "Weapon", "Apparel", "Crafting", "Ingredient", "Potion", "Scroll", "Quest", "Money" };
+
+        public Transform dropLocation;
+        [System.Serializable]
+        public struct Equipment
+        {
+            public string slotName;
+            public Transform equipLocation; //Character Rig loation for that item
+            public GameObject currentItem;
+        }
+        public Equipment[] equipmentSlots;
+
+        public static Chest.Inventory currentChest;
+        public static Shop.Inventory currentShop;
+        #endregion
+
         // Start is called before the first frame update
         void Start()
         {
-
+#if UNITY_EDITOR
+            playerInv.Add(ItemData.CreateItem(0));
+            playerInv.Add(ItemData.CreateItem(1));
+            playerInv.Add(ItemData.CreateItem(100));
+            playerInv.Add(ItemData.CreateItem(200));
+            playerInv.Add(ItemData.CreateItem(300));
+            playerInv.Add(ItemData.CreateItem(500));
+            playerInv.Add(ItemData.CreateItem(600));
+            playerInv.Add(ItemData.CreateItem(700));
+            playerInv.Add(ItemData.CreateItem(800));
+#endif
         }
 
         // Update is called once per frame
         void Update()
         {
+            //using keybinds
+            if (Input.GetKeyDown(KeyBinds.keys["Inventory"]))
+            {
+                //toggle inventory
+                //toggle mouselook
+                //toggle mouse
+                showInv = !showInv;
+                GameManager.gamePlayState = showInv ? GamePlayState.MenuPause : GamePlayState.Game;
+            }
 
+#if UNITY_EDITOR
+            if (Input.GetKey(KeyCode.KeypadPlus))
+            {
+                playerInv.Add(ItemData.CreateItem(0));
+                playerInv.Add(ItemData.CreateItem(1));
+                playerInv.Add(ItemData.CreateItem(100));
+                playerInv.Add(ItemData.CreateItem(200));
+                playerInv.Add(ItemData.CreateItem(300));
+                playerInv.Add(ItemData.CreateItem(500));
+                playerInv.Add(ItemData.CreateItem(600));
+                playerInv.Add(ItemData.CreateItem(700));
+                playerInv.Add(ItemData.CreateItem(800));
+            }
+#endif
+        }
+        void Display()
+        {
+            //if we want to display everything in our inventory
+            if (sortType == "All" | sortType == "")
+            {
+                //if we have 34 or less (space at top and bottom
+                if (playerInv.Count <= 34)
+                {
+                    for (int i = 0; i < playerInv.Count; i++)
+                    {
+                        if (GUI.Button(new Rect(0.5f * GameManager.scr.x, 0.25f * GameManager.scr.y + i * (0.25f * GameManager.scr.y), 3 * GameManager.scr.x, 0.25f * GameManager.scr.y), playerInv[i].Name))
+                        {
+                            selectedItem = playerInv[i];
+                        }
+                    }
+                }
+                //more then 34 items
+                else
+                {
+                    //our movable scroll position
+                    scrollPos =
+                    //the start of the viewable area
+                    GUI.BeginScrollView(
+                    //our view window
+                    new Rect(0, 0.25f * GameManager.scr.y, 3.75f * GameManager.scr.x, 8.5f * GameManager.scr.y),
+                    //our current scroll position within that window
+                    scrollPos,
+                    //scroll zone(extra space) that we can move withi the View Window
+                    new Rect(0, 0, 0, playerInv.Count * 0.25f * GameManager.scr.y),
+                    //can we see the horizontal bar?
+                    false,
+                    //can we see the vertical bar?
+                    true
+                    );
+                    #region Scroll View
+                    for (int i = 0; i < playerInv.Count; i++)
+                    {
+                        if (GUI.Button(new Rect(0.5f * GameManager.scr.x, i * (0.25f * GameManager.scr.y), 3 * GameManager.scr.x, 0.25f * GameManager.scr.y), playerInv[i].Name))
+                        {
+                            selectedItem = playerInv[i];
+                        }
+                    }
+                    #endregion
+                    //End the scroll space
+                    GUI.EndScrollView();
+                }
+            }
+            else //else we are displaying  based off type
+            {
+                ItemTypes type = (ItemTypes)System.Enum.Parse(typeof(ItemTypes), sortType);
+                //amount of that type
+                int a = 0;
+                //slot position
+                int s = 0;
+                for (int i = 0; i < playerInv.Count; i++)
+                {
+                    if (playerInv[i].Type == type)
+                    {
+                        a++;//increase a so we know how many we have
+                    }
+                }
+                //we are less than equal to 34 items of this type
+                if (a <= 34)
+                {
+                    for (int i = 0; i < playerInv.Count; i++)
+                    {
+                        if (playerInv[i].Type == type)
+                        {
+                            if (GUI.Button(new Rect(0.5f * GameManager.scr.x, 0.25f * GameManager.scr.y + s * (0.25f * GameManager.scr.y), 3 * GameManager.scr.x, 0.25f * GameManager.scr.y), playerInv[i].Name))
+                            {
+                                selectedItem = playerInv[i];
+                            }
+                            s++;
+                        }
+                    }
+                }
+                //we are greater than 34 items of this type
+                else
+                {
+                    scrollPos = GUI.BeginScrollView(new Rect(0, 0.25f * GameManager.scr.y, 3.75f * GameManager.scr.x, 8.5f * GameManager.scr.y), scrollPos, new Rect(0, 0, 0, a * 0.25f * GameManager.scr.y), false, true);
+                    #region Scroll View
+                    for (int i = 0; i < playerInv.Count; i++)
+                    {
+                        if (playerInv[i].Type == type)
+                        {
+                            if (GUI.Button(new Rect(0.5f * GameManager.scr.x, s * (0.25f * GameManager.scr.y), 3 * GameManager.scr.x, 0.25f * GameManager.scr.y), playerInv[i].Name))
+                            {
+                                selectedItem = playerInv[i];
+                            }
+                            s++;
+                        }
+                    }
+                    #endregion
+                    GUI.EndScrollView();
+                }
+            }
+        }
+
+        void UseItem()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+            if (showInv)
+            {
+                GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Inventory");
+                for (int i = 0; i < enumTypesForItems.Length; i++)
+                {
+                    if (GUI.Button(new Rect(4f * GameManager.scr.x + i * GameManager.scr.x, GameManager.scr.y * 0.5f, GameManager.scr.x, 0.25f * GameManager.scr.y), enumTypesForItems[i]))
+                    {
+                        sortType = enumTypesForItems[i];
+                    }
+                }
+                Display();
+                if (selectedItem != null)
+                {
+                    UseItem();
+                }
+            }
         }
     }
 }
